a8f08fcb6c466198b3a4ae156ae5492dbdfcfdd0
kai-jang99 <mmzz6699@gmail.com>
2024-12-08 00:39:59 +0900

    어드민-공지사항 작성기능 프론트엔드 구현
    공지내용 줄바꿈 안되는 문제 해결 / 라우팅을 위해 어드민라우츠 일부 수정 (작성페이지 디자인은 임시, 중요공지 5개 제한기능은 구현예정)

---
+++ src/routes/AdminRoutes.jsx
@@ -0,0 +1,57 @@
+import { lazy } from 'react';
+
+// project import
+import Loadable from 'components/Loadable';
+import Admin from 'layout/AdminLayout';
+
+// =====
+const Dashboard = Loadable(lazy(() => import('pages/admin/dashboard')));
+const Orders = Loadable(lazy(() => import('pages/admin/orders')));
+const Financial = Loadable(lazy(() => import('pages/admin/financial')));
+const Notice = Loadable(lazy(() => import('pages/admin/notice')));
+const MypageView = Loadable(lazy(() => import('pages/admin/mypage/MypageView')));
+const MypageUpdate = Loadable(lazy(() => import('pages/admin/mypage/MypageUpdate')));
+
+// ==============================|| ADMIN ROUTING ||============================== //
+
+const AdminRoutes = {
+  path: '/admin/*',
+  element: <Admin />,
+  children: [
+    {
+      path: '',
+      element: <Dashboard />
+    },
+    {
+      path: 'dashboard',
+      element: <Dashboard />
+    },
+    {
+      path: 'orders/*',
+      element: <Orders />
+    },
+    {
+      path: 'financial',
+      element: <Financial />
+    },
+    {
+      path: 'notice/*',
+      element: <Notice />
+    },
+    {
+      path: 'mypage',
+      children: [
+        {
+          path: 'view',
+          element: <MypageView />
+        },
+        {
+          path: 'update',
+          element: <MypageUpdate />
+        }
+      ]
+    }
+  ]
+};
+
+export default AdminRoutes;
