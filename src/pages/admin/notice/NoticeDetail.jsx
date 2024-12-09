3d8c60bd9900d64542549273b68de39e8d7cdf22
Kai Jang <mmzz6699@gmail.com>
2024-12-09 16:29:58 +0900

    공지 주석 추가

--- src/pages/admin/notice/NoticeDetail.jsx
+++ src/pages/admin/notice/NoticeDetail.jsx
@@ -1,18 +1,20 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Card, CardContent, Typography, Divider } from '@mui/material';
 import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 export default function NoticeDetail() {
+	const navigate = useNavigate();
 	const params = useParams();
 	const noticeNo = params.no;
-
 	const [notice, setNotice] = useState({});
 
 	useEffect(() => {
 		axios
-			.get(`http://localhost:8080/notice/${noticeNo}`)
+			.get(`http://localhost:8080/admin-notice/${noticeNo}`)
 			.then((response) => {
 				setNotice(response.data);
 				console.log(response.data);
@@ -52,14 +54,14 @@ export default function NoticeDetail() {
 			{/* Stack: 자식 요소들 사이 간격 2단위, 가로 방향 정렬, 상단 마진 20px, 중앙 정렬 */}
 			<Stack spacing={2} direction="row" sx={{ marginTop: '20px', justifyContent: 'center' }}>
 				{/* 이전 번호가 없으면 버튼 비활성화, 있으면 이전 버튼 클릭 가능 */}
-				<Button variant="contained" onClick={() => navigate(`/main/notice/${notice.prevNo}`)} disabled={!notice.prevNo}>
+				<Button variant="contained" onClick={() => navigate(`/admin/notice/${notice.prevNo}`)} disabled={!notice.prevNo}>
 					이전
 				</Button>
-				<Button variant="contained" onClick={() => navigate('/main/notice')}>
+				<Button variant="contained" onClick={() => navigate('/admin/notice')}>
 					목록
 				</Button>
 				{/* 다음 번호가 없으면 버튼 비활성화, 있으면 다음 버튼 클릭 가능 */}
-				<Button variant="contained" onClick={() => navigate(`/main/notice/${notice.nextNo}`)} disabled={!notice.nextNo}>
+				<Button variant="contained" onClick={() => navigate(`/admin/notice/${notice.nextNo}`)} disabled={!notice.nextNo}>
 					다음
 				</Button>
 			</Stack>
